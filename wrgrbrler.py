eed144e6b09f2645f07b2a9f1897880cf5119e50
turrino <matteo.turrino@abide-financial.com>
2016-05-20 21:02:00 +0100

    mixer

--- wrgrbrler.py
+++ wrgrbrler.py
@@ -1,18 +1,20 @@
 import argparse
+import random
 from PIL import Image, ImageFilter
 
 def main(main_args):
-    try:
-        original = Image.open("1.png")
-        blurred = original.filter(ImageFilter.BLUR)
 
-        original.show()
-        blurred.show()
-    except:
-        print("Unable to load image")
+    combined = Image.new('RGBA', (100,300), color=50)
 
+    part1 = Image.open("{0}.png".format(random.randrange(1, 9)))
+    part2 = Image.open("{0}.png".format(random.randrange(1, 9)))
+    part3 = Image.open("{0}.png".format(random.randrange(1, 9)))
 
+    combined.paste(part1, (0, 0))
+    combined.paste(part2, (0, 100))
+    combined.paste(part3, (0, 200))
 
+    combined.show()
 
 
 if __name__ == '__main__':
